ccda416512e0171464bdfdeba8e667812143e3e8
aliyun <hby_22@hnu.edu.cn>
2025-03-10 11:00:08 +0800

    init

--- Lab1/src/Sudoku/sudoku_basic.cc
+++ Lab1/src/Sudoku/sudoku_basic.cc
@@ -41,28 +41,28 @@ bool available(int guess, int cell)
 
 bool solve_sudoku_basic(int which_space)
 {
-  if (which_space >= nspaces) {
-    return true;
+  if (which_space >= nspaces) { // 如果当前空间索引大于等于总空间数
+    return true; // 返回true，表示解决成功
   }
 
   // find_min_arity(which_space);
-  int cell = spaces[which_space];
+  int cell = spaces[which_space]; // 获取当前空间索引对应的单元格
 
-  for (int guess = 1; guess <= NUM; ++guess) {
-    if (available(guess, cell)) {
+  for (int guess = 1; guess <= NUM; ++guess) { // 遍历1到NUM之间的所有可能值
+    if (available(guess, cell)) { // 如果当前值可用
       // hold
-      assert(board[cell] == 0);
-      board[cell] = guess;
+      assert(board[cell] == 0); // 断言当前单元格的值为0
+      board[cell] = guess; // 将当前单元格的值设置为guess
 
       // try
-      if (solve_sudoku_basic(which_space+1)) {
-        return true;
+      if (solve_sudoku_basic(which_space+1)) { // 递归调用solve_sudoku_basic函数，尝试解决下一个空间
+        return true; // 返回true，表示解决成功
       }
 
       // unhold
-      assert(board[cell] == guess);
-      board[cell] = 0;
+      assert(board[cell] == guess); // 断言当前单元格的值为guess
+      board[cell] = 0; // 将当前单元格的值设置为0
     }
   }
-  return false;
+  return false; // 返回false，表示解决失败
 }
--- Lab1/src/Sudoku/sudoku_min_arity.cc
+++ Lab1/src/Sudoku/sudoku_min_arity.cc
@@ -6,57 +6,65 @@
 
 static int arity(int cell)
 {
-  bool occupied[10] = {false};
-  for (int i = 0; i < NEIGHBOR; ++i) {
-    int neighbor = neighbors[cell][i];
-    occupied[board[neighbor]] = true;
-  }
-  return std::count(occupied+1, occupied+10, false);
+	bool occupied[10] = {false};
+	for (int i = 0; i < NEIGHBOR; ++i)
+	{
+		int neighbor = neighbors[cell][i];
+		occupied[board[neighbor]] = true;
+	}
+	return std::count(occupied + 1, occupied + 10, false);
 }
 
 static void find_min_arity(int space)
 {
-  int cell = spaces[space];
-  int min_space = space;
-  int min_arity = arity(cell);
+	int cell = spaces[space]; // 获取当前空间索引对应的单元格
+	int min_space = space; // 最小空间索引	
+	int min_arity = arity(cell); // 最小弧度
 
-  for (int sp = space+1; sp < nspaces && min_arity > 1; ++sp) {
-    int cur_arity = arity(spaces[sp]);
-    if (cur_arity < min_arity) {
-      min_arity = cur_arity;
-      min_space = sp;
-    }
-  }
+	for (int sp = space + 1; sp < nspaces && min_arity > 1; ++sp)
+	{
+		int cur_arity = arity(spaces[sp]); // 当前空间索引对应的弧度
+		if (cur_arity < min_arity) // 如果当前弧度小于最小弧度
+		{
+			min_arity = cur_arity;
+			min_space = sp;
+		}
+	}
 
-  if (space != min_space) {
-    std::swap(spaces[min_space], spaces[space]);
-  }
+	if (space != min_space)
+	{
+		std::swap(spaces[min_space], spaces[space]);
+	}
 }
 
 bool solve_sudoku_min_arity(int which_space)
 {
-  if (which_space >= nspaces) {
-    return true;
-  }
+	if (which_space >= nspaces)
+	{
+		return true;
+	}
 
-  find_min_arity(which_space);
-  int cell = spaces[which_space];
+	find_min_arity(which_space);	// 找到最小弧度
+	int cell = spaces[which_space]; // 获取当前空间索引对应的单元格
 
-  for (int guess = 1; guess <= NUM; ++guess) {
-    if (available(guess, cell)) {
-      // hold
-      assert(board[cell] == 0);
-      board[cell] = guess;
+	for (int guess = 1; guess <= NUM; ++guess)
+	{ // 遍历1到NUM之间的所有可能值
+		if (available(guess, cell))
+		{ // 如果当前值可用
+			// hold
+			assert(board[cell] == 0); // 断言当前单元格的值为0
+			board[cell] = guess;	  // 将当前单元格的值设置为guess
 
-      // try
-      if (solve_sudoku_min_arity(which_space+1)) {
-        return true;
-      }
+			// try
+			if (solve_sudoku_min_arity(which_space + 1))
+			{				 // 递归调用solve_sudoku_min_arity函数，尝试解决下一个空间
+				return true; // 返回true，表示解决成功
+			}
 
-      // unhold
-      assert(board[cell] == guess);
-      board[cell] = 0;
-    }
-  }
-  return false;
+			// unhold
+			assert(board[cell] == guess); // 断言当前单元格的值为guess
+			board[cell] = 0;			  // 将当前单元格的值设置为0
+		}
+	}
+	return false; // 返回false，表示解决失败
 }
